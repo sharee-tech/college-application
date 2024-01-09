47e894b0c02af864c3b0f5e294b7276bb54ec50c
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2024-01-09 14:10:15 -0600

    Update CollegeService.js

--- frontend/src/services/CollegeService.js
+++ frontend/src/services/CollegeService.js
@@ -9,6 +9,11 @@ const get = (id) => {
   return http.get(`/colleges/${id}`);
 };
 
+// Check if college exists in favorites for specific userId
+const getOne = (userId, collegeId) => {
+  return http.get(`/colleges/${userId}/${collegeId}`);
+};
+
 const create = (data) => {
   return http.post("/colleges", data);
 };
@@ -29,6 +34,7 @@ const remove = (userId, collegeId) => {
 
 const CollegeService = {
   getAll,
+  getOne,
   get,
   create,
   update,
