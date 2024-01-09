7ee328caa1019e08b7e7c109dc147db646e97206
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2024-01-09 14:08:54 -0600

    Update Favorites.js

--- frontend/src/routes/Favorites.js
+++ frontend/src/routes/Favorites.js
@@ -6,11 +6,8 @@ export default function Favorites() {
     <div>
       <main className="container">
         <h1>Welcome to Favorites</h1>
-        <div className="d-flex flex-row">
-          <div className="col-sm-12">
-            <FavoritesList />
-          </div>
-          {/* <div className="col-sm-4">Profile</div> */}
+        <div className="container">
+          <FavoritesList />
         </div>
       </main>
     </div>
