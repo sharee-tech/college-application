35eff27372c40927ab48df2c6e26c64e440b8a76
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2024-01-11 13:46:46 -0600

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,12 @@
-export default function Navbar() {
+import { NavLink } from "react-router-dom";
+
+export default function Navbar({ logout, user }) {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <NavLink className="navbar-brand" href="/">
           College EP
-        </a>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -19,28 +21,73 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/search">
+              <NavLink
+                to="/search"
+                activeClassName="active"
+                className="nav-link"
+              >
                 Search
-              </a>
+              </NavLink>
             </li>
-            <li className="nav-item">
-              <a
-                className="nav-link active"
-                aria-current="page"
-                href="/favorites"
-              >
-                Favorites
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link active"
-                aria-current="page"
-                href="/account"
-              >
-                Account
-              </a>
-            </li>
+
+            {user ? (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <NavLink
+                    to="/favorites"
+                    activeClassName="active"
+                    className="nav-link"
+                  >
+                    Favorites
+                  </NavLink>
+                </li>
+                <div className="collapse navbar-collapse justify-content-end">
+                  <ul className="navbar-nav ">
+                    <li className="nav-item">
+                      <NavLink
+                        to="/profile"
+                        activeClassName="active"
+                        className="nav-link"
+                      >
+                        {user.username}
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        to="/login"
+                        activeClassName="active"
+                        className="nav-link"
+                        onClick={logout}
+                      >
+                        Logout
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            ) : (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <NavLink
+                    to="/login"
+                    activeClassName="active"
+                    className="nav-link"
+                  >
+                    Login
+                  </NavLink>
+                </li>
+
+                <li className="nav-item">
+                  <NavLink
+                    to="/register"
+                    activeClassName="active"
+                    className="nav-link"
+                  >
+                    Signup
+                  </NavLink>
+                </li>
+              </div>
+            )}
           </ul>
         </div>
       </div>
