6713e57b900016b7b0702d00e138d606dfb032c2
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2024-01-11 19:18:05 -0600

    Update Navbar.js to right align login and logout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,71 +2,91 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar({ logout, user }) {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" href="/">
+        <NavLink className="navbar-brand" to="/">
           College EP
         </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav me-auto mb-2 mb-md-0">
-            <li className="nav-item">
-              <NavLink
-                to="/search"
-                activeClassName="active"
-                className="nav-link"
-              >
-                Search
-              </NavLink>
-            </li>
 
-            {user ? (
-              <div className="navbar-nav ml-auto">
+        {user ? (
+          <>
+            <div class="navbar-nav">
+              <li className="nav-item">
+                <NavLink
+                  to="/search"
+                  activeClassName="active"
+                  className="nav-link"
+                >
+                  Search
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to="/favorites"
+                  activeClassName="active"
+                  className="nav-link"
+                >
+                  Favorites
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  activeClassName="active"
+                  to="/contact"
+                >
+                  Contact Us
+                </NavLink>
+              </li>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav ms-auto">
                 <li className="nav-item">
                   <NavLink
-                    to="/favorites"
+                    to="/profile"
                     activeClassName="active"
                     className="nav-link"
                   >
-                    Favorites
+                    {user.username}
                   </NavLink>
                 </li>
-                <div className="collapse navbar-collapse justify-content-end">
-                  <ul className="navbar-nav ">
-                    <li className="nav-item">
-                      <NavLink
-                        to="/profile"
-                        activeClassName="active"
-                        className="nav-link"
-                      >
-                        {user.username}
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink
-                        to="/login"
-                        activeClassName="active"
-                        className="nav-link"
-                        onClick={logout}
-                      >
-                        Logout
-                      </NavLink>
-                    </li>
-                  </ul>
-                </div>
+                <li className="nav-item">
+                  <NavLink
+                    to="/login"
+                    activeClassName="active"
+                    className="nav-link"
+                    onClick={logout}
+                  >
+                    Logout
+                  </NavLink>
+                </li>
               </div>
-            ) : (
-              <div className="navbar-nav ml-auto">
+            </div>
+          </>
+        ) : (
+          <>
+            <div class="navbar-nav">
+              <li className="nav-item">
+                <NavLink
+                  to="/register"
+                  activeClassName="active"
+                  className="nav-link"
+                >
+                  Signup
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  activeClassName="active"
+                  to="/contact"
+                >
+                  Contact Us
+                </NavLink>
+              </li>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav ms-auto">
                 <li className="nav-item">
                   <NavLink
                     to="/login"
@@ -76,20 +96,10 @@ export default function Navbar({ logout, user }) {
                     Login
                   </NavLink>
                 </li>
-
-                <li className="nav-item">
-                  <NavLink
-                    to="/register"
-                    activeClassName="active"
-                    className="nav-link"
-                  >
-                    Signup
-                  </NavLink>
-                </li>
               </div>
-            )}
-          </ul>
-        </div>
+            </div>
+          </>
+        )}
       </div>
     </nav>
   );
