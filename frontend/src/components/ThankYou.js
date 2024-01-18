f5b1b458595724f2e5a50bc8599b9a4991b06872
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2024-01-18 11:50:39 -0600

    Create ThankYou.js

---
+++ frontend/src/components/ThankYou.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ThankYou = () => {
+  return (
+    <div>
+      <main className="container">
+        <div className="bg-light p-5 rounded">
+          <h1>Thank you!</h1>
+          <div className="d-flex flex-row">
+            <div className="col-sm-12">
+              <p className="lead">
+                The Development Team will be in contact shortly!
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default ThankYou;
