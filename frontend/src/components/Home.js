0a5884e32c70119641fd419d6d11bc7e566c805f
sharee-tech <84551254+sharee-tech@users.noreply.github.com>
2024-01-18 11:47:31 -0600

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,36 +1,37 @@
 import React, { useState, useEffect } from "react";
 
-// import UserService from "../services/user.service";
-
 function Home() {
-  // const [content, setContent] = useState("");
-
-  // useEffect(() => {
-  //   UserService.getPublicContent().then(
-  //     (response) => {
-  //       setContent(response.data);
-  //     },
-  //     (error) => {
-  //       const _content =
-  //         (error.response && error.response.data) ||
-  //         error.message ||
-  //         error.toString();
-
-  //       setContent(_content);
-  //     }
-  //   );
-  // }, []);
-
+  const authors = ` Sharee Thompson,
+                    Sharion Cranston,
+                    Andy Haag &
+                    Grayson Cummins`;
   return (
-    <main className="container">
-      <div className="bg-light p-5 rounded">
-        <h1>College Explorer Platform</h1>
-        <p className="lead">Blurb about College Explorer Platform...</p>
-        <a className="btn btn-lg btn-primary" href="/search" role="button">
-          Find Colleges Now &raquo;
-        </a>
+    <div>
+      <main className="container">
+        <div className="bg-light p-5 rounded">
+          <h1>College Explorer Platform</h1>
+          <p className="lead">
+            Welcome to College Explorer! This platform empowers you to
+            effortlessly discover and manage your favorite colleges. Start by
+            using the search feature to find colleges based on your preferences.
+            Save the ones you're interested in, compare them, or even edit with
+            personal notes. If you ever encounter any issues or have questions,
+            feel free to reach out to our team using the 'Contact Us' form.
+            Enjoy exploring your college options!
+          </p>
+          <a className="btn btn-lg btn-primary" href="/search" role="button">
+            Find Colleges Now &raquo;
+          </a>
+        </div>
+      </main>
+      <div className="container">
+        <div className="bg-light p-5 rounded">
+          <footer className="lead">
+            <p>Created by: {authors}</p>
+          </footer>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
 
